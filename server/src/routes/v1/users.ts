fbb46987b2e43ae946ea91413c903d63a68799ee
ahmedrafayat <ahmed.rafayat97@gmail.com>
2022-02-27 19:48:52 +0600

    Remove test route
    Remove the user delete test route

--- server/src/routes/v1/users.ts
+++ server/src/routes/v1/users.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express';
 
 import { follow, followers } from 'controllers/users';
-import { remove } from 'controllers/users/delete';
 import { validateFollowUser } from 'middlewares/request-validation/users';
 import { validateJwt } from 'middlewares/ValidateJwt';
 
@@ -9,7 +8,5 @@ const router = Router();
 
 router.post('/follow/:id', [validateJwt, validateFollowUser], follow);
 router.get('/followers', [validateJwt], followers);
-
-router.post('/delete/:id', [validateJwt], remove);
 
 export default router;
